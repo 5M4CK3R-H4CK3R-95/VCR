1a2b8c34703ab7219e58651fdc13211077ae29cc
5M4CK3R <62463436+5M4CK3R-H4CK3R-95@users.noreply.github.com>
2020-04-22 18:18:21 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@ echo "Please Wait While installing vcr .."
 sleep 2
 chmod +x *
 mv vcr /bin
-mv -r data /bin
+mv data /bin
 cd ..
 rm -rf VCR
 echo "Done Vcr is installed now just type vcr to start only run vcr there where you apk is .."
